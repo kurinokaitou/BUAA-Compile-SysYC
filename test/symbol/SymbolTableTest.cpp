746e7e282420d08f51a3399c523fa55bc6f2e3e1
kurino <gejingze@163.com>
2022-09-22 00:44:38 +0800

    chore: add unit test framework

---
+++ test/symbol/SymbolTableTest.cpp
@@ -0,0 +1,5 @@
+#include "SymbolTableTest.h"
+
+TEST_F(SymbolTableTest, test_name) {
+    EXPECT_EQ(1, 1);
+}
---
+++ test/symbol/SymbolTableTest.h
@@ -0,0 +1,21 @@
+#ifndef SYMBOL_TABLE_TEST_H
+#define SYMBOL_TABLE_TEST_H
+#include <gtest/gtest.h>
+#include <symbol/SymbolTable.h>
+
+class SymbolTableTest : public ::testing::Test {
+protected:
+    SymbolTableTest() = default;
+    virtual ~SymbolTableTest() = default;
+    virtual void SetUp(){};
+    virtual void TearDown(){};
+    static void SetUpTestSuite() {
+        std::cout << "Start testing symbol table" << std::endl;
+    }
+
+    static void TearDownTestSuite() {
+        std::cout << "End testing symbol table" << std::endl;
+    }
+    SymbolTable table;
+};
+#endif
